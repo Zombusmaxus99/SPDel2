d7421ab8f6c64402a8f2fec75979a6863f886ed3
GustavLberg <guttig50@gmail.com>
2024-09-25 12:27:46 +0200

    Added start menu options.

--- My project (1)/Assets/Script/MenuController.cs
+++ My project (1)/Assets/Script/MenuController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject levelMenu;
 
-    public void StartGame()
+    public void StartGame() //No longer nececary as it only loads one scene and LoadLevel is more flexible
     {
         SceneManager.LoadScene(1);
     }
@@ -15,6 +18,23 @@ public class MenuController : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LevelSelect() //Disables regular menu and enables level menu
+    {
+        mainMenu.SetActive(false);
+        levelMenu.SetActive(true);
+    }
+
+    public void ReturnToMain() //Disables level menu and enables main menu
+    {
+        mainMenu.SetActive(true);
+        levelMenu.SetActive(false);
+    }
+
+    public void LoadLevel(int sceneNumber) //Opens scene: sceneNumber
+    {
+        SceneManager.LoadScene(sceneNumber);
     }
 
     public void ShowCredits()
