3f10be7a4c9044926587f84a1bc973793b97585e
Zombusmaxus99 <ionicav522@gmail.com>
2024-10-02 05:03:14 +0200

    Final commit
    Everything is put together

--- My project (1)/Assets/Script/MenuController.cs
+++ My project (1)/Assets/Script/MenuController.cs
@@ -1,16 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private GameObject creditsPanel;
-    [SerializeField] private GameObject mainMenu;
-    [SerializeField] private GameObject levelMenu;
 
-    public void StartGame() //No longer nececary as it only loads one scene and LoadLevel is more flexible
+    public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
@@ -18,23 +15,6 @@ public class MenuController : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void LevelSelect() //Disables regular menu and enables level menu
-    {
-        mainMenu.SetActive(false);
-        levelMenu.SetActive(true);
-    }
-
-    public void ReturnToMain() //Disables level menu and enables main menu
-    {
-        mainMenu.SetActive(true);
-        levelMenu.SetActive(false);
-    }
-
-    public void LoadLevel(int sceneNumber) //Opens scene: sceneNumber
-    {
-        SceneManager.LoadScene(sceneNumber);
     }
 
     public void ShowCredits()
--- My project (1)/Assets/Script/PlayerMovement.cs
+++ My project (1)/Assets/Script/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform leftFoot, rightFoot;
     [SerializeField] private Transform spawnPosition;
     [SerializeField] private LayerMask whatIsGround;
-    [SerializeField] private AudioClip jumpSound, pickupSound, powerupSound;
+    [SerializeField] private AudioClip jumpSound, pickupSound, powerupSound, hitSound;
     [SerializeField] private GameObject melonParticles,dustParticles;
 
     [SerializeField] private Slider healthSlider;
@@ -19,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Color greenHealth, redHealth;
     [SerializeField] private TMP_Text melonText;
 
+    public Transform currentSpawnPoint;
     private float horizontalValue;
     private float rayDistance = 0.25f;
     private bool isGrounded;
@@ -47,6 +48,7 @@ public class PlayerMovement : MonoBehaviour
         rend = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+        currentSpawnPoint = spawnPosition;
     }
 
 
@@ -154,6 +156,8 @@ public class PlayerMovement : MonoBehaviour
         
         currentHealth -= damageAmount;
         UpdateHealthBar();
+        audioSource.pitch = Random.Range(0.8f, 1.2f);
+        audioSource.PlayOneShot(hitSound, 0.5f);
 
         if (currentHealth <= 0)
         {
@@ -177,8 +181,8 @@ public class PlayerMovement : MonoBehaviour
     {
         currentHealth = startingHealth;
         UpdateHealthBar();
-        transform.position = spawnPosition.position;
         rgbd.velocity = Vector2.zero;
+        transform.position = currentSpawnPoint.position;
     }
 
     private void RestoreHealth(GameObject healthPickup)
